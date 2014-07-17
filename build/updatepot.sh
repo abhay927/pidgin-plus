a5c25819d22f0c24baa276c2c87d9201436dfab2
Renato Silva <br.renatosilva@gmail.com>
2014-07-17 13:17:58 -0300

    Update translation template before adding new string in next commit.

--- build/updatepot.sh
+++ build/updatepot.sh
@@ -11,4 +11,4 @@ fi
 
 cd ../source/po
 intltool_home=$(readlink -e "$1/win32-dev/intltool_"*)
-PATH="$PATH:$intltool_home/bin" XGETTEXT_ARGS=--no-location intltool-update --pot
+PATH="$PATH:$intltool_home/bin" XGETTEXT_ARGS="--no-location --sort-output" intltool-update --pot
