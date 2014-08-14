057afb94a14bb95cb2281fd4f75c0a1e0c530cc3
Renato Silva <br.renatosilva@gmail.com>
2014-08-14 02:17:57 -0300

    Improvements to the Windows build script.
    * Split GnuPG and Authenticode code signing into two separate options.
    * Exit the script if any of the make commands fail.

--- build/winbuild.sh
+++ build/winbuild.sh
@@ -24,10 +24,15 @@
 ##                          branch. Requires Bazaar.
 ##     -c, --cleanup        Clean up the staging dir then exit.
 ##
-##         --sign=FILE      Enable code signing with Microsoft Authenticode and
-##                          GnuPG, using FILE as the PKCS #12 / PFX certificate
-##                          for Authenticode. Both GnuPG and the signtool
-##                          utility must be available from system path.
+##         --sign           Enable code signing with GnuPG. This applies to the
+##                          source code, dictionary and GTK+ bundles, as well as
+##                          to the installers. Requires GnuPG.
+##
+##         --cert=FILE      Enable code signing with Microsoft Authenticode,
+##                          using FILE as the PKCS #12 / PFX certificate. This
+##                          applies to the main executable and the installers.
+##                          Requires the signtool utility from Windows SDK.
+##                          Implies --sign.
 ##
 ##     -r, --reset          Recreates the staging directory from scratch.
 ##         --staging=DIR    Staging directory, defaults to "pidgin.build".
@@ -41,27 +46,29 @@ eval "$(from="$0" easyoptions.rb "$@"; echo result=$?)"
 [[ ! -d "${arguments[0]}" || $result != 0 ]] && exit
 
 # Variables and functions
+sign="${sign:-$cert}"
+sign="${sign:+yes}"
 devroot="${arguments[0]}"
 version=$(./changelog.sh --version)
 staging="$devroot/${staging:-pidgin.build}"
 target="${directory:-$devroot/distribution/$version}"
 windev="$devroot/win32-dev/pidgin-windev.sh"
-build() { make -f Makefile.mingw "$1" SIGNTOOL_PASSWORD="$pfx_password" GPG_PASSWORD="$gpg_password" "${@:2}"; }
+build() { make -f Makefile.mingw "$1" SIGNTOOL_PASSWORD="$pfx_password" GPG_PASSWORD="$gpg_password" "${@:2}" || exit 1; }
 
-# Prepare for code signing
+# GnuPG version and password
 if [[ -n "$sign" ]]; then
     gpg_version=$(gpg --version | head -1)
     gpg_version="${gpg_version##* }"
     [[ "$gpg_version" = 1.* ]] && hash gpg2 2> /dev/null && gpg=gpg2
-
-    # GnuPG password
     read -s -p "Enter password for GnuPG: " gpg_password; echo
     $gpg --batch --yes --passphrase "$gpg_password" --output /tmp/test.asc -ab "$0" || exit 1
     rm /tmp/test.asc
+fi
 
-    # Authenticode password
-    read -s -p "Enter password for $sign: " pfx_password; echo
-    openssl pkcs12 -in "$sign" -nodes -password "pass:$pfx_password" > /dev/null || exit 1
+# Authenticode password
+if [[ -n "$cert" ]]; then
+    read -s -p "Enter password for Authenticode: " pfx_password; echo
+    openssl pkcs12 -in "$cert" -nodes -password "pass:$pfx_password" > /dev/null || exit 1
 fi
 
 # Pidgin Windev
@@ -77,8 +84,8 @@ fi
 if [[ -n "$cleanup" ]]; then
     if [[ -d "$staging" ]]; then
         cd "$staging"
-        make -f Makefile.mingw uninstall
-        make -f Makefile.mingw clean
+        build uninstall
+        build clean
     else
         echo "Nothing to clean up."
     fi
@@ -101,10 +108,12 @@ eval $("$windev" "$devroot" --path --system-gcc)
 cd "$staging"
 
 # Code signing
-if [[ -n "$sign" ]]; then
+if [[ -n "$cert" ]]; then
     rm local.mak
-    echo "SIGNTOOL_PFX = $sign" >> local.mak
+    echo "SIGNTOOL_PFX = $cert" >> local.mak
     echo "GPG_SIGN = $gpg" >> local.mak
+elif [[ -n "$sign" ]]; then
+    sed -i "s/^GPG_SIGN.*/GPG_SIGN = $gpg/" local.mak
 fi
 
 # GTK+ and dictionary bundles
@@ -143,12 +152,12 @@ if [[ -n "$source" ]]; then
 fi
 
 # Installers
-build "installer${offline:+s}" || exit
+build "installer${offline:+s}"
 for asc in "" ${sign:+.asc}; do
     [[ -n "$offline" ]] && mv -v pidgin-*-offline.exe$asc "$target/Pidgin $version Offline Setup.exe$asc"
     mv -v pidgin-*.exe$asc "$target/Pidgin $version Setup.exe$asc"
     mv -v pidgin-*-dbgsym.zip$asc "$target/Pidgin Debug Symbols $version.zip$asc"
 done
-make -f Makefile.mingw uninstall
+build uninstall
 echo "Build finished."
 cd - > /dev/null
